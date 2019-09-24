d00bddbf6986dd6fc3d60d30b03373f9f6a5b6ed
dlanier <lanier4@illinois.edu>
2019-09-24 16:06:32 -0500

    initial yaml test

--- src/im_scale_products.py
+++ src/im_scale_products.py
@@ -237,11 +237,19 @@ def now_name(prefi_str=None, suffi_str=None):
     return prefi_str + '_' + ahora_nombre + suffi_str
 
 def scaled_images_dataset(run_parameters):
-    n_2_do = run_parameters['n_2_do']
-    iteration_dict = run_parameters['iteration_dict']
-    small_scale = run_parameters['run_parameters']
-    large_scale = run_parameters['large_scale']
-    output_dir = run_parameters['output_dir']
+    """ assemble input arguments and call scaled_images_dataset() function
+    Args:
+        run_parameters:   input arguments for write_n_image_sets
+
+    Returns:
+        nothing:
+    """
+    number_of_image_sets = run_parameters['number_of_image_sets']
+    it_max = run_parameters['it_max']
+    scale_dist = run_parameters['scale_dist']
+    small_scale = [run_parameters['small_scale_rows'], run_parameters['small_scale_cols']]
+    large_scale = [run_parameters['large_scale_rows'], run_parameters['large_scale_cols']]
+    results_directory = run_parameters['results_directory']
 
     if 'hash_list' in run_parameters:
         hash_list = run_parameters['hash_list']
@@ -249,35 +257,42 @@ def scaled_images_dataset(run_parameters):
     else:
         hash_list = []
 
-    hash_list = write_n_image_sets(n_2_do, iteration_dict, small_scale, large_scale, output_dir, hash_list)
+    hash_list = write_n_image_sets(number_of_image_sets,
+                                   it_max,
+                                   scale_dist,
+                                   small_scale,
+                                   large_scale,
+                                   results_directory,
+                                   hash_list)
 
     print('\n%i pairs written \n'%(len(hash_list)))
 
-def write_n_image_sets(n_2_do, iteration_dict, small_scale, large_scale, output_dir, hash_list=None):
-    """
+def write_n_image_sets(number_of_image_sets, it_max, scale_dist, small_scale, large_scale, results_directory, hash_list):
+    """ calculate and write a set of unique images at two scales
     Args:
-        n_2_do:         number of pairs of images
-        iteration_dict: {'it_max': 64, 'scale_dist': 10} escape-time-algorithm iteration limits
-        small_scale:    [h, w] for smaller copy of image
-        large_scale:    [h, w] for larger copy of image
-        output_dir:     directory will be written if DNE
-        hash_list       empty list []  OR  list returned by this function - to avoid duplicates
+        number_of_image_sets:   number of pairs of images
+        it_max:                 maximum number of iterations
+        scale_dist:             escape boundry in units of domain diagonal distance
+        small_scale:            [h, w] for smaller copy of image
+        large_scale:            [h, w] for larger copy of image
+        results_directory:      directory will be written if DNE
+        hash_list               empty list []  OR  list returned by this function - to avoid duplicates
         
     Returns:
-        hash_list:      list of parameter encodings to insure uniqueness 
-                        if producing large dataset over multiple sessions or locations
+        hash_list:          list of parameter encodings to insure uniqueness
+                            (if producing large dataset over multiple sessions or locations)
                         
-    Writes n_2_do image pairs like:
+    Writes number_of_image_sets image pairs like:
                         asdregshaaldfkaproiapw_small.jpg
                         asdregshaaldfkaproiapw_large.jpg
     """
     if hash_list is None:
         hash_list = []
         
-    if os.path.isdir(output_dir) == False:
-        os.makedirs(output_dir)
+    if os.path.isdir(results_directory) == False:
+        os.makedirs(results_directory)
                 
-    print(now_name('Write %i image pairs to \n%s\nStart '%(n_2_do, output_dir)))
+    print(now_name('Write %i image pairs to \n%s\nStart '%(number_of_image_sets, results_directory)))
     
     if len(hash_list) > 0:
         print('checking duplicates using input hash_list size = %i'%(len(hash_list)))
@@ -286,12 +301,12 @@ def write_n_image_sets(n_2_do, iteration_dict, small_scale, large_scale, output_
 
     with TemporaryDirectory() as test_temporary_dir:
 
-        for k_do in range(n_2_do):
+        for k_do in range(number_of_image_sets):
             fcn_name, eq, p = get_rand_eq_p_set()
             domain_dict = get_random_domain()
 
-            domain_dict['it_max'] = iteration_dict['it_max']
-            domain_dict['max_d'] = iteration_dict['scale_dist'] / domain_dict['zoom']
+            domain_dict['it_max'] = it_max
+            domain_dict['max_d'] = scale_dist / domain_dict['zoom']
 
             hash_idx = hash_parameters(domain_dict, fcn_name, p)
             if hash_idx in hash_list:
@@ -307,7 +322,7 @@ def write_n_image_sets(n_2_do, iteration_dict, small_scale, large_scale, output_
                 t0 = time.time()
                 ET, Z, Z0 = eq_iter.get_primitives(list_tuple, domain_dict)
                 I = get_im(ET, Z, Z0, domain_dict)
-                file_name = os.path.join(output_dir, hash_idx + '_' + 'small.jpg')
+                file_name = os.path.join(results_directory, hash_idx + '_' + 'small.jpg')
                 I.save(file_name)
 
                 domain_dict['n_rows'] = large_scale[0]
@@ -315,9 +330,9 @@ def write_n_image_sets(n_2_do, iteration_dict, small_scale, large_scale, output_
 
                 ET, Z, Z0 = eq_iter.get_primitives(list_tuple, domain_dict)
                 I = get_im(ET, Z, Z0, domain_dict)
-                file_name = os.path.join(output_dir, hash_idx + '_' + 'large.jpg')
+                file_name = os.path.join(results_directory, hash_idx + '_' + 'large.jpg')
                 I.save(file_name)
-                print('\n%3i of %3i) %s\t\t'%(k_do+1, n_2_do, fcn_name), 
+                print('\n%3i of %3i) %s\t\t'%(k_do+1, number_of_image_sets, fcn_name),
                       '%0.3f seconds (large & small image written)\n'%(time.time() - t0), 
                       hash_idx)
                 
